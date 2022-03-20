fd7d0b684b7f884a1b940d3ea49bf69d20e4d817
yeezze <86194303+yeezze@users.noreply.github.com>
2022-03-20 13:51:09 +0900

    Feat: 기본 도형 그리기

--- src/pattern/DrawingPanel.java
+++ src/pattern/DrawingPanel.java
@@ -7,5 +7,21 @@ public class DrawingPanel extends JPanel {
     private static final long serialVersionUTD = 1L;
 
     public DrawingPanel() {
+        this.setBackground(Color.white);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+
+        graphics.drawRect(20, 20, 40, 40);
+        graphics.fillOval(20, 20, 40, 40);
+        graphics.drawLine(20, 20, 40, 40);
+
+        graphics.setColor(Color.RED);
+        int xPoints[] = {20, 40, 60};
+        int yPoints[] = {20, 49, 60};
+        int nPoints = 3;
+        graphics.drawPolygon(xPoints, yPoints, nPoints);
     }
 }
--- src/pattern/MainFrame.java
+++ src/pattern/MainFrame.java
@@ -12,16 +12,17 @@ public class MainFrame extends JFrame {
 
     public MainFrame() {
         this.setSize(400,600);
-        this.setLayout(new FlowLayout());
+        BorderLayout layoutManager = new BorderLayout();
+        this.setLayout(layoutManager);
 
         this.menuBar = new MenuBar();
         this.setJMenuBar(this.menuBar);
 
         this.toolBar = new ToolBar();
-        this.add(this.toolBar);
+        this.add(this.toolBar, layoutManager.NORTH);
 
         this.drawingPanel = new DrawingPanel();
-        this.add(this.drawingPanel);
+        this.add(this.drawingPanel, layoutManager.CENTER);
 
         setTitle("my window");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
