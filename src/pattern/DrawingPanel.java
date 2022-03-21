31043de8580ffb76b89bd3b96d144b29eadd2975
yeezze <86194303+yeezze@users.noreply.github.com>
2022-03-21 23:06:06 +0900

    MouseHandler 추가

--- src/pattern/DrawingPanel.java
+++ src/pattern/DrawingPanel.java
@@ -1,27 +1,63 @@
 package pattern;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
 
-public class DrawingPanel extends JPanel {
+public class DrawingPanel extends JPanel{
     private static final long serialVersionUTD = 1L;
 
     public DrawingPanel() {
         this.setBackground(Color.white);
+
+        MouseHandler mouseHandler = new MouseHandler();
+        // button
+        this.addMouseListener(mouseHandler);
+        // position
+        this.addMouseMotionListener(mouseHandler);
     }
 
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
+    }
 
-        graphics.drawRect(20, 20, 40, 40);
-        graphics.fillOval(20, 20, 40, 40);
-        graphics.drawLine(20, 20, 40, 40);
+    private class MouseHandler implements MouseInputListener {
+        @Override
+        public void mouseClicked(MouseEvent e) {
 
-        graphics.setColor(Color.RED);
-        int xPoints[] = {20, 40, 60};
-        int yPoints[] = {20, 49, 60};
-        int nPoints = 3;
-        graphics.drawPolygon(xPoints, yPoints, nPoints);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 }
--- src/pattern/ToolBar.java
+++ src/pattern/ToolBar.java
@@ -1,14 +1,18 @@
 package pattern;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ToolBar extends JToolBar {
+public class ToolBar extends JToolBar implements ActionListener {
     private static final long serialVersionUTD = 1L;
 
     private JRadioButton rectangleTool;
     private JRadioButton ovalTool;
     private JRadioButton lineTool;
     private JRadioButton polygonTool;
+    private JButton colorButton;
 
     public ToolBar() {
         ButtonGroup buttonGroup = new ButtonGroup();
@@ -28,5 +32,16 @@ public class ToolBar extends JToolBar {
         this.polygonTool = new JRadioButton("polygon");
         this.add(this.polygonTool);
         buttonGroup.add(this.polygonTool);
+
+        this.colorButton = new JButton("색상 선택");
+        this.colorButton.addActionListener(this);
+        this.add(colorButton);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == colorButton) {
+            JColorChooser colorChooser = new JColorChooser();
+            Color color = JColorChooser.showDialog(null, "색상 고르기", Color.black);
+        }
     }
 }
