da826617b6714d23aa31e175ddae2005c86ed8da
yeezze <86194303+yeezze@users.noreply.github.com>
2022-03-23 11:44:51 +0900

    0323 수업 : Rectangle 그리기

--- src/pattern/DrawingPanel.java
+++ src/pattern/DrawingPanel.java
@@ -24,53 +24,77 @@ public class DrawingPanel extends JPanel{
         super.paint(graphics);
     }
 
+    Rectangle rectangle;
+
+    private void prepareDrawing(int x, int y) {
+        this.rectangle = new Rectangle(x, y);
+    }
+
+    private void keepDrawing(int x, int y) {
+        // erase (배경색을 다시 그린다)
+        this.rectangle.draw(this.getGraphics());
+        // draw (그림을 그리는 역할은?? : 그래픽스!)
+        this.rectangle.resize(x, y);
+        this.rectangle.draw(this.getGraphics()); // 패널이 가지고 있는 그래픽스를 가져옴
+    }
+
+    private void finishDrawing(int x, int y) {
+    }
+
+    private class Rectangle {
+        private int x, y, width, height;
+
+        public Rectangle(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.width = 0;
+            this.height = 0;
+        }
+
+        public void resize(int x, int y) {
+            this.width = x - this.x;
+            this.height = y - this.y;
+        }
+
+        public void draw(Graphics graphics) {
+            graphics.drawRect(this.x, this.y, width, height);
+        }
+    }
+
     private class MouseHandler implements MouseListener, MouseMotionListener, MouseWheelListener {
         @Override
         public void mouseClicked(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "(" + e.getButton() + ") : " + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY() + ": " + e.getClickCount());
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            prepareDrawing(e.getX(), e.getY());
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            finishDrawing(e.getX(), e.getY());
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            keepDrawing(e.getX(), e.getY());
         }
 
         @Override
         public void mouseMoved(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseWheelMoved(MouseWheelEvent e) {
-            String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0] + " : " + e.getWheelRotation() + ", " + e.getScrollAmount());
         }
     }
 }
