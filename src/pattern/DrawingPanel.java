0f4c9ce5d67a751771472d57e7b33492f43874cd
yeezze <86194303+yeezze@users.noreply.github.com>
2022-03-23 11:06:33 +0900

    0323 수업 : Mouse Event

--- src/pattern/DrawingPanel.java
+++ src/pattern/DrawingPanel.java
@@ -1,10 +1,8 @@
 package pattern;
 
 import javax.swing.*;
-import javax.swing.event.MouseInputListener;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.geom.AffineTransform;
+import java.awt.event.*;
 
 public class DrawingPanel extends JPanel{
     private static final long serialVersionUTD = 1L;
@@ -17,6 +15,8 @@ public class DrawingPanel extends JPanel{
         this.addMouseListener(mouseHandler);
         // position
         this.addMouseMotionListener(mouseHandler);
+        // wheel
+        this.addMouseWheelListener(mouseHandler);
     }
 
     @Override
@@ -24,54 +24,53 @@ public class DrawingPanel extends JPanel{
         super.paint(graphics);
     }
 
-    private class MouseHandler implements MouseInputListener {
+    private class MouseHandler implements MouseListener, MouseMotionListener, MouseWheelListener {
         @Override
         public void mouseClicked(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "(" + e.getButton() + ") : " + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY() + ": " + e.getClickCount());
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseMoved(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
             String[] eNameArr = e.paramString().split(",");
-            System.out.println(eNameArr[0]);
-            System.out.println("x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+            System.out.println(eNameArr[0] + "x좌표 : " + e.getX() + ", y좌표 : " +e.getY());
+        }
+
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            String[] eNameArr = e.paramString().split(",");
+            System.out.println(eNameArr[0] + " : " + e.getWheelRotation() + ", " + e.getScrollAmount());
         }
     }
 }
