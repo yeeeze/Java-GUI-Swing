87602aadbf7dd0ff19cf087372693963962cf6f4
yeezze <86194303+yeezze@users.noreply.github.com>
2022-04-13 18:10:52 +0900

    Refactor 과제: ETools를 이용하여 ToolBar의 Button 일반화하기

--- src/pattern/frames/ToolBar.java
+++ src/pattern/frames/ToolBar.java
@@ -2,22 +2,18 @@ package pattern.frames;
 
 import javax.swing.*;
 
-import pattern.global.Constants;
 import pattern.global.Constants.ETools;
-import pattern.shapes.*;
 
-import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToolBar extends JToolBar {
     // attributes
     private static final long serialVersionUTD = 1L;
 
     // components
-    private JRadioButton rectangleTool;
-    private JRadioButton ovalTool;
-    private JRadioButton lineTool;
-    private JRadioButton polygonTool;
+    private List<JRadioButton> buttonList = new ArrayList<>();
 
     // associations
     private DrawingPanel drawingPanel;
@@ -28,36 +24,17 @@ public class ToolBar extends JToolBar {
         ActionHandler actionHandler = new ActionHandler();
 
         for(ETools eTool: ETools.values()) {
-            // 버튼의 갯수를 어떻게 알지?? 반복문 횟수말이야
+            buttonList.add(new JRadioButton(eTool.getButtonName()));
+            this.buttonList.get(eTool.ordinal()).setActionCommand(eTool.name());
+            this.buttonList.get(eTool.ordinal()).addActionListener(actionHandler);
+            this.add(this.buttonList.get(eTool.ordinal()));
+            buttonGroup.add(this.buttonList.get(eTool.ordinal()));
         }
-        this.rectangleTool = new JRadioButton("rectangle");
-        this.rectangleTool.setActionCommand(ETools.eRectangle.name());
-        this.rectangleTool.addActionListener(actionHandler);
-        this.add(this.rectangleTool);
-        buttonGroup.add(this.rectangleTool);
-
-        this.ovalTool = new JRadioButton("oval");
-        this.ovalTool.setActionCommand(ETools.eOval.name());
-        this.ovalTool.addActionListener(actionHandler);
-        this.add(this.ovalTool);
-        buttonGroup.add(this.ovalTool);
-
-        this.lineTool = new JRadioButton("line");
-        this.lineTool.setActionCommand(ETools.eLine.name());
-        this.lineTool.addActionListener(actionHandler);
-        this.add(this.lineTool);
-        buttonGroup.add(this.lineTool);
-
-        this.polygonTool = new JRadioButton("polygon");
-        this.polygonTool.setActionCommand(ETools.ePolygon.name());
-        this.polygonTool.addActionListener(actionHandler);
-        this.add(this.polygonTool);
-        buttonGroup.add(this.polygonTool);
     }
 
     public void associate(DrawingPanel drawingPanel) {
         this.drawingPanel = drawingPanel;
-        this.rectangleTool.doClick();
+        this.buttonList.get(ETools.eRectangle.ordinal()).doClick();
     }
 
     private class ActionHandler implements ActionListener {
--- src/pattern/global/Constants.java
+++ src/pattern/global/Constants.java
@@ -5,19 +5,25 @@ import pattern.shapes.*;
 public class Constants {
 
     public enum ETools {
-        eRectangle(new TRectangle()),
-        eOval(new TOval()),
-        eLine(new TLine()),
-        ePolygon(new TPolygon());
+        eRectangle(new TRectangle(), "rectangle"),
+        eOval(new TOval(), "oval"),
+        eLine(new TLine(), "line"),
+        ePolygon(new TPolygon(), "polygon");
 
         private TShape tool;
+        private String buttonName;
 
-        ETools(TShape tool) {
+        ETools(TShape tool, String buttonName) {
             this.tool = tool;
+            this.buttonName = buttonName;
         }
 
         public TShape newShape() {
             return this.tool.clone();
+        }
+
+        public String getButtonName() {
+            return buttonName;
         }
     }
 }
